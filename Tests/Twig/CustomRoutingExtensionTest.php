7664311fb348ebb690214623c29e6809a77d4976
Emmanuel BORGES <contact@eborges.fr>
2018-04-03 21:12:53 +0200

    update composer.json

--- Tests/Twig/CustomRoutingExtensionTest.php
+++ Tests/Twig/CustomRoutingExtensionTest.php
@@ -1,12 +1,16 @@
 <?php
 
-use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
+namespace EB78\CustomI18nRouterBundle\Tests\Twig;
+
+use Mockery as m;
+use PHPUnit\Framework\TestCase;
 
 class CustomRoutingExtensionTest extends TestCase
 {
+    use m\Adapter\Phpunit\MockeryPHPUnitIntegration;
+
     public function setUp()
     {
-
     }
 
     public function testGetPathShouldReturnAnUri()
